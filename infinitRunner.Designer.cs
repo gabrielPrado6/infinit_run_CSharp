6612da8611e3d0a2a7ef42fc5d9f9fa9d3b1e69d
Gabriel Prado <33374300+gabrielPrado6@users.noreply.github.com>
2019-01-09 11:25:03 -0200

    Código fonte do jogo de corrida infinita
    Código com especificações do projeto para livre consulta e adaptação, caso esse código venha a te ajudar, peço que lembre-se de mim nos créditos, ficaria imensamente grato.

---
+++ infinitRunner.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class infinitRunner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Enabled = true;
+            this.gameLoop.Interval = 33;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.player.Location = new System.Drawing.Point(124, 221);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 100);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.LawnGreen;
+            this.ground.Location = new System.Drawing.Point(2, 420);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(782, 141);
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(696, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(699, 35);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Olive;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 46);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // infinitRunner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.player);
+            this.Name = "infinitRunner";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.runner_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ infinitRunner.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class infinitRunner : Form
+    {
+
+
+        //Variaveis de uso continuo para retocar ou mudar o jogo
+        public static int
+                        jumpTime = 7,
+                        jumpPower = 20,
+                        moveSpeed = 30,
+                        gravitation = 15,
+                        timerWold = 0,
+                        timerObstacle = 0,
+                        timerCoin = 0,
+                        obstaclesDelay = 20,
+                        points = 0;
+        
+        //Função para criar numeros aleatórios 
+        public Random rnd = new Random();
+
+        //Classe do Player onde acontecerá as alterações e interações
+        public class Player
+        {
+            //Variaveis da Classe
+            public bool onAir = true;
+            public PictureBox actor;
+            private int topVector = 0;
+            private int time = 0;
+            
+            //Metodo construtor
+            public Player(PictureBox setActor)
+            {
+                actor = setActor;
+            }
+
+            //Função Set da Variael OnAir
+            public void setOnAir(bool setAir)
+            {
+                onAir = setAir;
+            }
+            //Função Get da Variavel OnAir
+            public bool getOnAir()
+            {
+                return onAir;
+            }
+
+            //Função get Simplificada da Variavel Actor
+            public PictureBox Actor
+            {
+                get { return actor; }
+            }
+
+            //Função get e set Simplificada da Variavel TopVector
+            public int TopVector
+            {
+                get { return topVector; }
+                set { topVector = value; }
+            }
+
+            //Função para mover o jogador ao pular
+            public void move()
+            {
+                //se o valor de TopVector for diferrente de 0
+                if(topVector != 0)
+                {
+                    onAir = true;
+                    //Incrementa o Valor para cima equivalente a 
+                    //actor.top = actor.top + topVector;
+                    actor.Top -= topVector;
+                    //Incrementa o time do Player
+                    time++;
+                }
+
+                //Se o tempo do jogador for maior ou igual ao tempo de pulo 
+                if (time >= jumpTime)
+                {
+                    //Zera os valores 
+                    topVector = 0;
+                    time = 0;
+                }
+            }
+        }
+
+        //Função Game Over para parar o jogo
+        public void gameOver()
+        {
+            gameLoop.Stop();
+        }
+
+        //Lista de Colisores para calcular as interações
+        public List<PictureBox> obstacles = new List<PictureBox>();
+
+        //Criação do player
+        Player gamePlayer;
+
+        public infinitRunner()
+        {
+            InitializeComponent();
+            //Criar o Player com o metodo construtor
+            gamePlayer = new Player(player);
+        }
+
+        //Evento que ocorre toda vez que o jogador pressiona
+        private void runner_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Se o jogador apertar espaço e o player não estiver no ar
+            if (e.KeyCode == Keys.Space && !gamePlayer.getOnAir())
+            {
+                //Muda o topVector para poder de pulo + gravidade
+                gamePlayer.TopVector = jumpPower+gravitation;
+            }
+
+            //Se o jogador apertar espaço e o jogo tiver acabado ou parado
+            if (e.KeyCode == Keys.Space && gameLoop.Enabled == false)
+            {
+                obstacles.Clear();
+                //Reinicia a aplicação
+                Application.Restart();
+            }
+        }
+
+        //Evento do Timer: gameLoop, ocore quando passa um ciclo
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            //Gravidade do mundo 
+            if (gamePlayer.getOnAir() == true)
+            {
+                gamePlayer.Actor.Top += gravitation;
+            }
+
+            
+            //Se o jogador intercede o chão
+            if (gamePlayer.Actor.Bounds.IntersectsWith(ground.Bounds))
+            {
+                gamePlayer.setOnAir(false);
+
+            }
+            else
+            {
+                gamePlayer.setOnAir(true);
+            }
+
+            //Movimento
+            gamePlayer.move();
+
+            if(obstacles.Count > 20)
+            {
+                //Destroi o obstaculo obsoletos
+                obstacles[0].Dispose();
+                obstacles.Remove(obstacles[0]);
+            }
+
+            //Colisão de Obstaculos e Moedas
+            for(int i = 0; i < obstacles.Count; i++)
+            {
+                //Move o obstaculo
+                obstacles[i].Left -= moveSpeed;
+                //Se o obstaculo colidir com o Player
+                if (obstacles[i].Bounds.IntersectsWith(gamePlayer.actor.Bounds))
+                {  
+                    //Se o item for uma moeda
+                    if(obstacles[i].BackColor == Color.Yellow)
+                    {   
+                        //Ganha  pontos
+                        points += 10;
+                        //Destroi o obstaculo
+                        obstacles[i].Dispose();
+                        obstacles.Remove(obstacles[i]);
+                    }
+                    //Se não acaba o jogo
+                    else
+                    {
+                        gameOver();
+                    }
+                }
+            }
+
+            //cria obstaculos
+            //Cria um tempo para o proximo obstaculo caso tempo seja 0
+            if (timerObstacle == 0)
+            {
+                timerObstacle = rnd.Next(obstaclesDelay, obstaclesDelay * 2);
+            }
+
+            //Se o tempo do mundo for igual ao dos obstaculos ele cria um obstaculo
+            if (timerWold == timerObstacle)
+            {
+                //Variaveis para criar um novo obstaculo
+                int obsType = rnd.Next(2);
+                PictureBox newObs = new PictureBox();
+
+                //Variação entre dois tipos de obstaculos
+                if (obsType == 0)
+                {
+                    //Criando uma picturesBox nova
+                    newObs = new PictureBox
+                    {
+                        Size = new Size(50, 80),
+                        Location = new Point(800, 340),
+                        BackColor = Color.Red
+                    };
+                }
+                else
+                {
+                    //Criando uma picturesBox nova
+                    newObs = new PictureBox
+                    {
+                        Size = new Size(120, 50),
+                        Location = new Point(800, 372),
+                        BackColor = Color.Red
+                    };
+                }
+
+                //Adicionar a nova pictureBox ao formulario
+                this.Controls.Add(newObs);
+                //Adiciona a picture Box a lista de obstaculos
+                obstacles.Add(newObs);
+                //Zera o tempo do mundo e dos obstaculos
+                timerObstacle = 0;
+                timerWold = 0;
+            }
+
+            //cria Moeda
+            //Cria um tempo para o proximo obstaculo caso tempo seja 0
+            if (timerCoin == 0)
+            {
+                timerCoin= rnd.Next(obstaclesDelay/2, obstaclesDelay);
+            }
+
+            //Se o tempo do mundo for igual ao dos obstaculos ele cria um obstaculo
+            if (timerWold == timerCoin)
+            {
+                //Criando uma picturesBox nova
+                PictureBox newObs = new PictureBox
+                {
+                    Size = new Size(50, 50),
+                    Location = new Point(800, 200),
+                    BackColor = Color.Yellow
+                };
+                
+                //Adicionar a nova pictureBox ao formulario
+                this.Controls.Add(newObs);
+                //Adiciona a picture Box a lista de obstaculos
+                obstacles.Add(newObs);
+                //Zera o tempo do mundo e dos obstaculos
+                timerCoin = 0;
+            }
+
+            //Incrementa os tempo
+            timerWold++;
+
+            //Exibe as informações em labels
+            label1.Text = "timer: " + timerWold.ToString();
+            label2.Text = "colisores: " + obstacles.Count.ToString();
+            label3.Text = points.ToString() + "  pontos";
+        }
+    }
+}
